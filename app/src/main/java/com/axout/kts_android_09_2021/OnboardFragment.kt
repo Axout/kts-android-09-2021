6b22da2d3d4bd8f8658536249f8d941467a09e46
Sergey Rakov <rako-serge@yandex.ru>
2021-10-02 23:02:41 +0300

    add onboarding with viewPager2

--- app/src/main/java/com/axout/kts_android_09_2021/OnboardFragment.kt
+++ app/src/main/java/com/axout/kts_android_09_2021/OnboardFragment.kt
@@ -3,18 +3,26 @@ package com.axout.kts_android_09_2021
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
-import androidx.navigation.fragment.findNavController
+import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.axout.kts_android_09_2021.databinding.FragmentOnboardBinding
+import com.axout.kts_android_09_2021.onboarding.OnboardAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class OnboardFragment : Fragment(R.layout.fragment_onboard) {
 
     private val binding by viewBinding(FragmentOnboardBinding::bind)
+    private lateinit var onboardNamesArray: Array<String>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.btOnLoginFrag.setOnClickListener {
-            val action = OnboardFragmentDirections.actionOnboardFragmentToLoginFragment()
-            findNavController().navigate(action)
-        }
+
+        onboardNamesArray = resources.getStringArray(R.array.onboard_names)
+
+        val onboardAdapter = OnboardAdapter(activity as AppCompatActivity, onboardNamesArray.size)
+        binding.onboardViewPager.adapter = onboardAdapter
+
+        TabLayoutMediator(binding.tabLayout, binding.onboardViewPager) { tab, position ->
+            //Some implementation
+        }.attach()
     }
 }
---
+++ app/src/main/java/com/axout/kts_android_09_2021/onboarding/OnboardingFr.kt
@@ -0,0 +1,65 @@
+package com.axout.kts_android_09_2021.onboarding
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.axout.kts_android_09_2021.OnboardFragmentDirections
+import com.axout.kts_android_09_2021.R
+import kotlinx.android.synthetic.main.fr_onboarding.*
+import java.io.IOException
+
+class OnboardingFr : Fragment(R.layout.fr_onboarding) {
+
+    companion object {
+        const val ARG_POSITION = "position"
+
+        fun getInstance(position: Int): Fragment {
+            val onboardingFr = OnboardingFr()
+            val bundle = Bundle()
+            bundle.putInt(ARG_POSITION, position)
+            onboardingFr.arguments = bundle
+            return onboardingFr
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val position = requireArguments().getInt(ARG_POSITION)
+        val imageFilePath = getString(R.string.onboard_image_path, position)
+        val onboardNamesArray = requireContext().resources.getStringArray(R.array.onboard_names)
+
+        setImageFromAssetsFile(requireContext(), imageFilePath)
+        tv_onboardingName.text = onboardNamesArray[position]
+
+        if (position == onboardNamesArray.size - 1) {
+            bt_onLoginFrag.visibility = View.VISIBLE
+            bt_onLoginFrag.setOnClickListener {
+                val action = OnboardFragmentDirections.actionOnboardFragmentToLoginFragment()
+                findNavController().navigate(action)
+            }
+        }
+    }
+
+    /**
+     * Gets the file from assets, converts it into a bitmap and sets it on the ImageView
+     * @param context a Context instance
+     * @param filePath relative path of the file
+     */
+    private fun setImageFromAssetsFile(context: Context, filePath: String) {
+        val imageBitmap: Bitmap?
+        val assets = context.resources.assets
+        try {
+            val fileStream = assets.open(filePath)
+            imageBitmap = BitmapFactory.decodeStream(fileStream)
+            fileStream.close()
+            iv_onboarding.setImageBitmap(imageBitmap)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(context, getString(R.string.image_loading_error), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
