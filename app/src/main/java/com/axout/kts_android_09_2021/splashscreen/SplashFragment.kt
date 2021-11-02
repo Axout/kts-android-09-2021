2911a26b2550616a24617a5b5266983fb7ff1477
Sergey Rakov <60923967+Axout@users.noreply.github.com>
2021-11-02 21:12:35 +0300

    fix crash when splashscreen rotated

--- app/src/main/java/com/axout/kts_android_09_2021/splashscreen/SplashFragment.kt
+++ app/src/main/java/com/axout/kts_android_09_2021/splashscreen/SplashFragment.kt
@@ -1,5 +1,6 @@
 package com.axout.kts_android_09_2021.splashscreen
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.os.Handler
 import androidx.fragment.app.Fragment
@@ -14,11 +15,13 @@ class SplashFragment : Fragment(R.layout.fragment_splash) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_NOSENSOR
 
         handler = Handler()
         handler.postDelayed({
             val action = SplashFragmentDirections.actionSplashFragmentToOnboardFragment()
             findNavController().navigate(action)
+            activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
         }, 3000)
     }
 }
