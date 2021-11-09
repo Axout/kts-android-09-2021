a674551e0a87ef143f57c6306998ac0f25ae215f
Sergey Rakov <60923967+Axout@users.noreply.github.com>
2021-11-09 22:21:20 +0300

    add bind profile data

--- app/src/main/java/com/axout/kts_android_09_2021/profile/ProfileFragment.kt
+++ app/src/main/java/com/axout/kts_android_09_2021/profile/ProfileFragment.kt
@@ -3,20 +3,37 @@ package com.axout.kts_android_09_2021.profile
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.axout.kts_android_09_2021.R
 import com.axout.kts_android_09_2021.databinding.FragmentProfileBinding
+import com.axout.kts_android_09_2021.main.models.DataModel
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
+    private val dataModel: DataModel by activityViewModels()
     private val binding by viewBinding(FragmentProfileBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        bindViewModel()
+
         binding.topAppBar.setNavigationOnClickListener {
             findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentPop())
         }
+    }
+
+    private fun bindViewModel() {
+        (dataModel.firstname.value + " " + dataModel.lastname.value).also { binding.tvAuthor.text = it }
+
+        Glide.with(this)
+            .load(dataModel.profile.value)
+            .transform(CircleCrop())
+            .placeholder(R.drawable.avatar_m)
+            .into(binding.ivAvatar)
     }
 }
